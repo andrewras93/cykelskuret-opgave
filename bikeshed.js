d81ada811ea4cc86822a2b332c9cb520e6644f06
Andrew <andrewras93@gmail.com>
2021-02-12 10:37:44 +0100

    You can now filter bike list by type of bike

--- bikeshed.js
+++ bikeshed.js
@@ -1,4 +1,5 @@
 let bikes = [];
+let filters = [];
 const bikeForm = document.getElementById('bikeForm');
 const bikeName = document.getElementById('bikeName');
 const bikeList = document.getElementById('bikeList');
@@ -6,85 +7,126 @@ const formErrMsg = document.getElementById('formErrMsg');
 const bikeGear = document.getElementById('bikeGear');
 const bikeType = document.getElementById('bikeType');
 const bikePrice = document.getElementById('bikePrice');
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/format
 const options = { style: 'currency', currency: 'DKK' };
 const numberFormat = new Intl.NumberFormat('da-DA', options);
-
 const sortBtn = document.getElementById('sortBtn');
 const listSort = document.getElementById('listSort');
+const filterBtn = document.getElementById('filterBtn');
+const mountainBike = document.getElementById('mountainBike');
+const cityBike = document.getElementById('cityBike');
+const tandemBike = document.getElementById('tandemBike');
 
 function addBike(e){
+// Hvad der skal ske når vi trykker 'Tilføj Cykel'.
 
     e.preventDefault();
 
     if(!bikeName.value || !bikePrice.value){
+    // Hvis bikeName ikke har en værdi eller bikePrice ikke har en værdi, skal følgende ske:
 
         formErrMsg.innerHTML = 'Udfyld venligst begge felter';
 
-    } else {
+    }
 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    else {
+     // Ellers skal følgende ske:
+
+        formErrMsg.innerHTML = '';
+
         const date = new Date();
+        // Tager dato og tid, på det tidspunkt der blev trykket på 'Tilføj Cykel'.
 
         const bike = {
-            bikeID: bikes.length,
+        // Vores cykel objekt.
+
+            bikeID: bikes.length, // Tager længden på vores bikes array, som er tom fra start dvs. id'et på vores første cykel vil være '0'.
             bikeName: bikeName.value,
             bikeGear: bikeGear.value,
             bikeType: bikeType.value,
-            bikePrice: numberFormat.format(bikePrice.value),
+            bikePrice: bikePrice.value,
             date: date
         }
 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-        /*if(bikes.find(element => element.bikeName === bikeName.value)){
-            bikeNameError.innerHTML = 'Cyklen findes allerede';
-            return
-        }*/
+        bikes.push(bike); // Indsætter vores bike objekt, med alle dets værdier, til sidst i vores bikes array.
+        // Vi har altså nu tilføjet en cykel til vores bikes array.
 
-        bikes.push(bike);
+        bikeName.value = ''; // Vi ryder det der står i input feltet til bikeName, så man er klar til evt. at indtaste navnet på en ny cykel.
+        bikePrice.value = ''; // Det samme gøres for feltet til bikePrice, så man kan indtaste en ny pris.
+        bikeName.focus(); // Sætter bikeName inputtet i fokus, så man ikke skal klikke på det igen, for at indtaste cykel navn.
 
-        formErrMsg.innerHTML = '';
-        bikeName.value = '';
-        bikeName.focus();
-        bikePrice.value = '';
-
-        updateBikes();
+        updateBikes(); // Kalder/kører en funktion ved navn updateBikes.
 
     }
 
 }
 
-function updateBikes(){
+function updateBikes() {
 
     bikeList.innerHTML = '';
 
     bikes.forEach(function (bike){
+    // Tager og 'kigger' igennem vores bikes array. For hvert element (bike) vi har i vores array, skal følgende kode køres, på det pågældende element.
 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
-        const dateFormat = { year: 'numeric', month: 'numeric', day: 'numeric' };
+        if( filters.length === 0 || filters.includes(bike.bikeType)){
+        // Hvis længden på vores filter array er lig med 0, hvilket vil sige det er tomt (hvilket det er fra start af),
+        // eller hvis det inkluderer en type af cykel, skal følgende ske:
 
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`Cykel: ID: ${bike.bikeID}, Navn: ${bike.bikeName}, Antal gear: ${bike.bikeGear}, Type: ${bike.bikeType}, Pris: ${bike.bikePrice}, Dato: ${bike.date.toLocaleDateString(undefined, dateFormat)} `));
+            const dateFormat = { year: 'numeric', month: 'numeric', day: 'numeric' }; // Formaterer vores date variabel, så vi kan udskrive det pænt med år, måned og dag angivet som tal.
+            const li = document.createElement('li'); // For hvert element i vores bikes array skabes der et '<li></li>' tag.
+            const span = document.createElement('span'); // Udover vores '<li>' tag, bliver der også tilføjet et '<span></span>' tag for hvert element.
 
-        const span = document.createElement('span');
-        span.appendChild(document.createTextNode(`X`));
-        span.classList.add('remove');
+            li.appendChild(document.createTextNode(`Cykel: ID: ${bike.bikeID}, Navn: ${bike.bikeName}, Antal gear: ${bike.bikeGear}, Type: ${bike.bikeType}, Pris: ${numberFormat.format(bike.bikePrice)}, Dato: ${bike.date.toLocaleDateString(undefined, dateFormat)} `));
+            // Vi tilføjer en 'TextNode' til vores '<li>' tag, hvor vi skriver værdierne for hvert element (de enkelte cyklers værdier, som vi har fået fra vores input felter (bortset fra date)).
+            li.appendChild(span); // Tilføjer også 'span' til vores 'li'.
 
-        span.addEventListener('click', () => {
+            span.appendChild(document.createTextNode(`X`)); // Vi tilføjer endnu engang en 'TextNode', denne gang til vores '<span>', hvor vi skriver et simpelt kryds.
+            span.classList.add('remove'); // Tilføjer en klasse til vores '<span>'.
 
-            li.remove();
+            bikeList.appendChild(li); // Udskriver vores cykler til 'dokumentet', ved at tilføje dem til vores bikeList '<ul>'.
 
-            bikes = bikes.filter(b => b.bikeID !== bike.bikeID)
+            span.addEventListener('click', () => {
+            // For hvert element tilføjes en 'EventListener'. Hvis elementets 'span' klikkes på, skal følgende ske:
 
-        });
+                li.remove(); // Det pågælende elements 'li' og dens indhold fjernes (inkluderer også 'span'), men kun fra selve 'dokumentet' og ikke fra vores array.
 
-        li.appendChild(span);
+                bikes = bikes.filter(b => b.bikeID !== bike.bikeID)
+                // Vi filtrerer igennem vores bikes array og laver et nyt array, med de cykler hvor id'et ikke matcher det der er blevet klikket på.
+                // Dvs. det tager et element (b) ud, kigger på det elements id og holder det oppe imod det 'originale' id, matches de ikke, bliver det element (b) sat i det nye array.
+                // Hvis det skulle skrives primitivt:
+                /*let newBikes = [];
 
-        bikeList.appendChild(li);
+                for (let i = 0; i < bikes.length; i++){
+                    if (bikes[i].bikeID !== bike.bikeID){
+                        newBikes.push(bikes[i]);
+                    }
+                }
+
+                bikes = newBikes;*/
+
+            });
+
+        }
 
     });
 }
+
+filterBtn.addEventListener('click', function (){
+
+    filters = [];
+
+    if(mountainBike.checked){
+        filters.push(mountainBike.value);
+    }
+    if (cityBike.checked){
+        filters.push(cityBike.value);
+    }
+    if (tandemBike.checked){
+        filters.push(tandemBike.value);
+    }
+
+    updateBikes();
+
+});
 
 sortBtn.addEventListener('click', function (){
 
@@ -109,9 +151,8 @@ sortBtn.addEventListener('click', function (){
         updateBikes();
     }
 
-})
+});
 
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
 function sortGear(){
     bikes.sort(function (bike1, bike2) {
         return bike1.bikeGear - bike2.bikeGear;
@@ -171,3 +212,12 @@ if(bikeForm){
     console.log(`The ID of bikeForm doesn't exist.`);
 
 }
+
+/* KILDER:
+
+Number format: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/format
+Date: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+Array find: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+Array sort: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+
+ */
