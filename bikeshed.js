e667f8bd746714ec4d438ed8526a34bbb9f975d7
Andrew <andrewras93@gmail.com>
2021-02-22 10:07:30 +0100

    Removed some out commented code

--- bikeshed.js
+++ bikeshed.js
@@ -52,7 +52,6 @@ class DataBase {
 
 //END OF DATABASE SECTION
 
-let bikes = [];
 let filters = [];
 const bikeForm = document.getElementById('bikeForm');
 const bikeName = document.getElementById('bikeName');
@@ -75,103 +74,59 @@ updateBikes(); //Initialize first update if database had items from local storag
 
 function addBike(e){
 
-// Hvad der skal ske når vi trykker 'Tilføj Cykel'.
-
     e.preventDefault();
 
     if(!bikeName.value || !bikePrice.value){
-    // Hvis bikeName ikke har en værdi eller bikePrice ikke har en værdi, skal følgende ske:
 
         formErrMsg.innerHTML = 'Udfyld venligst begge felter';
 
     }
-
     else {
-     // Ellers skal følgende ske:
 
         formErrMsg.innerHTML = '';
 
         const date = new Date();
-        // Tager dato og tid, på det tidspunkt der blev trykket på 'Tilføj Cykel'.
- 
-        
-        const bike = {
-        // Vores cykel objekt.
-
-            bikeID: bikes.length, // Tager længden på vores bikes array, som er tom fra start dvs. id'et på vores første cykel vil være '0'.
-            bikeName: bikeName.value,
-            bikeGear: bikeGear.value,
-            bikeType: bikeType.value,
-            bikePrice: bikePrice.value,
-            date: date
-        }
 
         database.add(bikeName.value, bikeGear.value, bikeType.value, bikePrice.value, date)
 
-        //bikes.push(bike); // Indsætter vores bike objekt, med alle dets værdier, til sidst i vores bikes array.
-        // Vi har altså nu tilføjet en cykel til vores bikes array.
+        bikeName.value = '';
+        bikePrice.value = '';
+        bikeName.focus();
 
-        bikeName.value = ''; // Vi ryder det der står i input feltet til bikeName, så man er klar til evt. at indtaste navnet på en ny cykel.
-        bikePrice.value = ''; // Det samme gøres for feltet til bikePrice, så man kan indtaste en ny pris.
-        bikeName.focus(); // Sætter bikeName inputtet i fokus, så man ikke skal klikke på det igen, for at indtaste cykel navn.
-
-        updateBikes(); // Kalder/kører en funktion ved navn updateBikes.
+        updateBikes();
 
     }
-
 }
 
 function updateBikes() {
 
     bikeList.innerHTML = '';
 
-    bikes = database.items;
+    database.items.forEach(function (bike) {
 
-    bikes.forEach(function (bike){
-    // Tager og 'kigger' igennem vores bikes array. For hvert element (bike) vi har i vores array, skal følgende kode køres, på det pågældende element.
+        if ( filters.length === 0 || filters.includes(bike.type)) {
 
-        if( filters.length === 0 || filters.includes(bike.type)){
-        // Hvis længden på vores filter array er lig med 0, hvilket vil sige det er tomt (hvilket det er fra start af),
-        // eller hvis det inkluderer en type af cykel, skal følgende ske:
-
-            const dateFormat = { year: 'numeric', month: 'numeric', day: 'numeric' }; // Formaterer vores date variabel, så vi kan udskrive det pænt med år, måned og dag angivet som tal.
-            const li = document.createElement('li'); // For hvert element i vores bikes array skabes der et '<li></li>' tag.
-            const span = document.createElement('span'); // Udover vores '<li>' tag, bliver der også tilføjet et '<span></span>' tag for hvert element.
+            const dateFormat = { year: 'numeric', month: 'numeric', day: 'numeric' };
+            const li = document.createElement('li');
+            const span = document.createElement('span');
 
             li.appendChild(document.createTextNode(`Cykel: ID: ${bike.id}, Navn: ${bike.name}, Antal gear: ${bike.gear}, Type: ${bike.type}, Pris: ${numberFormat.format(bike.price)}, Dato: ${bike.date.toLocaleDateString(undefined, dateFormat)} `));
-            // Vi tilføjer en 'TextNode' til vores '<li>' tag, hvor vi skriver værdierne for hvert element (de enkelte cyklers værdier, som vi har fået fra vores input felter (bortset fra date)).
-            li.appendChild(span); // Tilføjer også 'span' til vores 'li'.
+            li.appendChild(span);
 
-            span.appendChild(document.createTextNode(`X`)); // Vi tilføjer endnu engang en 'TextNode', denne gang til vores '<span>', hvor vi skriver et simpelt kryds.
-            span.classList.add('remove'); // Tilføjer en klasse til vores '<span>'.
+            span.appendChild(document.createTextNode(`X`));
+            span.classList.add('remove');
 
-            bikeList.appendChild(li); // Udskriver vores cykler til 'dokumentet', ved at tilføje dem til vores bikeList '<ul>'.
+            bikeList.appendChild(li);
 
             span.addEventListener('click', () => {
-            // For hvert element tilføjes en 'EventListener'. Hvis elementets 'span' klikkes på, skal følgende ske:
 
-                li.remove(); // Det pågælende elements 'li' og dens indhold fjernes (inkluderer også 'span'), men kun fra selve 'dokumentet' og ikke fra vores array.
-
+                li.remove();
                 database.remove(bike.id);
 
                 //bikes = bikes.filter(b => b.bikeID !== bike.bikeID)
-                // Vi filtrerer igennem vores bikes array og laver et nyt array, med de cykler hvor id'et ikke matcher det der er blevet klikket på.
-                // Dvs. det tager et element (b) ud, kigger på det elements id og holder det oppe imod det 'originale' id, matches de ikke, bliver det element (b) sat i det nye array.
-                // Hvis det skulle skrives primitivt:
-                /*let newBikes = [];
-
-                for (let i = 0; i < bikes.length; i++){
-                    if (bikes[i].bikeID !== bike.bikeID){
-                        newBikes.push(bikes[i]);
-                    }
-                }
-
-                bikes = newBikes;*/
 
             });
-
         }
-
     });
 }
 
